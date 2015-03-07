7d5b026fb32ea967d50f1b7583847d695b2fddc1
Gerald Ding <geralddzx@gmail.com>
2015-03-07 14:37:21 -0500

    deflator

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module Scbt
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.middleware.use Rack::Deflater
     config.filepicker_rails.api_key = ENV["FILE_PICKER_API_KEY"]
     config.assets.initialize_on_precompile = false
   end
